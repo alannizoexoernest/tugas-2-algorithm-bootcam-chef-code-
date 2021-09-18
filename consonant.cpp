0349288f5b639977e69352fe1ab9542b9a06c046
alannizoexoernest <90753843+alannizoexoernest@users.noreply.github.com>
2021-09-18 17:02:19 +0700

    Add files via upload

---
+++ consonant.cpp
@@ -0,0 +1,15 @@
+//Al Annizo Exo Ernest
+#include <stdio.h>
+
+int main()
+{
+ char C;
+scanf("%s", &C);
+if(C=='A' || C=='E' || C=='I' || C=='O' || C=='U'){
+  printf("Vowel");
+}
+else{
+  printf("Consonant");
+}
+return 0;
+}
